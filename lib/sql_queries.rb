a89cb83b7039acb81ca04d1610c232e3f61d944d
AT Williams <aderiannaw@yahoo.com>
2018-08-14 02:23:28 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -36,7 +36,8 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 SELECT  users.name, SUM(pledges.amount)
 FROM
 JOIN
-GROUP BY
+GROUP BY user.name
+ORDER BY user.name 
 HAVING
 "
 end
